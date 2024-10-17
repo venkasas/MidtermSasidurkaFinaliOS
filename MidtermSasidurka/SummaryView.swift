9cb88abff0a97970cb3d9f9fa5006ca584b04f2c
Sasidurka Venkatesan <venkasas@sheridancollege.ca>
2024-10-17 01:31:43 -0400

    first view commit

---
+++ MidtermSasidurka/SummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryView.swift
+//  MidtermSasidurka
+//
+//  Created by Sasidurka on 2024-10-17.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SummaryView()
+}
