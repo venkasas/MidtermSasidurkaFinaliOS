51e7784186a577a1d59d110928130dc82fe011cd
Sasidurka Venkatesan <venkasas@sheridancollege.ca>
2024-10-17 02:18:05 -0400

    summary view created

--- MidtermSasidurka/SummaryView.swift
+++ MidtermSasidurka/SummaryView.swift
@@ -8,36 +8,11 @@
 import SwiftUI
 
 struct SummaryView: View {
-    
-    @Binding var orders: [Order] // Use Binding to access the orders
-    
     var body: some View {
-        VStack {
-            List {
-                ForEach(orders.indices, id: \.self) { index in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("Size: \(orders[index].size)")
-                            Text("Type: \(orders[index].toppings)")
-                            
-                            Text("Quantity: \(orders[index].quantity)")
-                        }
-                    }
-                }
-            }
-            .listStyle(PlainListStyle())
-        }
-        .navigationTitle("My Orders")
-        .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct OrderListView_Previews: PreviewProvider {
-    @State static var previewOrders = [
-        Order(size: "Medium", toppings: "Pepperoni", crust: "Thin", quantity: 2)
-    ]
-    
-    static var previews: some View {
-        SummaryView(orders: $previewOrders)
-    }
+#Preview {
+    SummaryView()
 }
