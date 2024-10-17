c3f0696370dd91dec7128e63f7b82da7ce513358
Sasidurka Venkatesan <venkasas@sheridancollege.ca>
2024-10-17 00:33:14 -0400

    Initial Commit

---
+++ MidtermSasidurka/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MidtermSasidurka
+//
+//  Created by Sasidurka on 2024-10-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MidtermSasidurka/MidtermSasidurkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermSasidurkaApp.swift
+//  MidtermSasidurka
+//
+//  Created by Sasidurka on 2024-10-17.
+//
+
+import SwiftUI
+
+@main
+struct MidtermSasidurkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
